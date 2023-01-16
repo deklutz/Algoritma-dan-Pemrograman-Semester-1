825ac348ff318d98e50182e1486ea5e1598f0421
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-01-16 07:02:42 +0700

    Update biodata for.cpp

--- biodata for.cpp
+++ biodata for.cpp
@@ -7,7 +7,6 @@ int main()
       for (i = 1; i<=1; i++)
       { cout << "Nama: Hanindita Rifdah \n";
       	cout << "NIM: 6702223020 \n";
-      	cout << "Kelas: D3TK-46-01 \n";
       	cout << "Prodi: D3 Teknologi Komputer \n";
 	  }
       return 0;
